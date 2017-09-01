cdb82ea904c9a800bcda437fae34ec7bb673ce16
MPQ <mpq@MPQ.local>
2017-09-01 17:09:39 +0200

    SimpleBookManager

--- app/src/main/java/com/mobilecomputing/bookmanager/MainActivity.java
+++ app/src/main/java/com/mobilecomputing/bookmanager/MainActivity.java
@@ -2,6 +2,10 @@ package com.mobilecomputing.bookmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import com.mobilecomputing.bookmanager.Models.Book;
+import com.mobilecomputing.bookmanager.Models.BookManager;
+import com.mobilecomputing.bookmanager.Models.SimpleBookManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
